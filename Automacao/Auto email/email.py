1d40ab3c01eee241791602f1358f86df7cf08b03
Hmarcall <hmarcal826@gmail.com>
2025-05-30 16:37:27 -0300

    1.17

--- Automacao/Auto email/email.py
+++ Automacao/Auto email/email.py
@@ -4,4 +4,29 @@ import webbrowser
 import pyperclip
 
 webbrowser.open("https://mail.google.com")
-time.sleep(3)
+time.sleep(4)
+
+pyautogui.click(x=280, y=550)
+time.sleep(2)
+
+with open("Automacao/Auto email/email.txt", "r", encoding="utf-8") as f:
+    linhas = f.readlines()
+
+destinatario = linhas[0].split(":", 1)[1].strip()
+assunto = linhas[1].split(":", 1)[1].strip()
+mensagem = linhas[2].split(":", 1)[1].strip()
+
+pyperclip.copy(destinatario)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("tab")
+time.sleep(1)
+
+pyperclip.copy(assunto)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("tab")
+time.sleep(1)
+
+pyperclip.copy(mensagem)
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.hotkey("ctrl", "enter")
